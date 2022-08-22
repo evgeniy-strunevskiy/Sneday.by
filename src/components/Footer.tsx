f33c83be8fc6253726f41592a6aac03d398b9360
Evgeniy Strunevskiy <esstrunevskiy@gmail.com>
2022-08-22 15:03:23 +0300

    Компонент Footer. Правки

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,6 +6,6 @@ const cl = classNames.bind(styles);
 
 export const Footer: FC = () => {
   return (
-    <footer className={cl('footer')}>Сайт придуман и реализован Евгением Струневским</footer>
+    <footer className={cl('footer')}>Автор и разработчик сайта: Евгений Струневский</footer>
   )
 }
